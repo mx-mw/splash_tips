f4ffe2fa5c0d0117c3bcbf21be5963a7a0df9700
max <87119201+mx-mw@users.noreply.github.com>
2025-05-30 15:33:58 +1000

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+import "./src/env/index.js"
+
+const nextConfig = {
+  // if you want to use standalone output, uncomment the following line
+  // transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+};
+
+export default nextConfig;
